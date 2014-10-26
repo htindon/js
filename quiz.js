81366af25184bc370b54724544471881e1f1e3cb
Helene Tindon <htindon@student.42.fr>
2014-10-26 18:08:37 +0100

    working on the quiz, a lot of issues to fix

---
+++ quiz.js
@@ -0,0 +1,45 @@
+var quiz = function () {
+
+	var compareArrays = function(array1, array2) {
+		var i = 0;
+		while (i < array1.length || i < array2.length) {
+			if (array1[i] != array2[i]) {
+				return false;
+			}
+			i++;
+		}
+		return true;
+	}
+
+	var explainErrors = function (user, answers, possible) {
+		var i = 0;
+		var explanation = "";
+		while (i < possible.length) {
+			if (user[i] === "false" && answers[i] === "true") {
+				explanation.push("Answer " + document.getElementById(possible[i]).name + ": " + document.getElementById(possible[i]).text + "was true.");
+			} else if (user[i] != answers[i]) {
+				explanation.push("Answer " + document.getElementById(possible[i]).name + ": " + document.getElementById(possible[i]).text + "was false.");
+			}
+		}
+		document.getElementById("answer_quiz").innerHTML = explanation;
+	}
+
+	var userAnswers = [];
+	var trueAnswers = ["true", "false", "false", "false"];
+	var possibleAnswers = ["a", "b", "c", "d"];
+	var i = 0;
+	while (i < possibleAnswers.length) {
+		if (document.getElementById(possibleAnswers[i]).checked) {
+			userAnswers.push("true");
+		} else {
+			userAnswers.push("false");
+		}
+		i++;
+	}
+	if (compareArrays(trueAnswers, userAnswers)) {
+		document.getElementById("answer_quiz").innerHTML = "Congratulations, your answers were right.";
+	} else {
+		explainErrors(userAnswers, trueAnswers, possibleAnswers);
+	}
+	document.close();
+};
