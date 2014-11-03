e6c3ec7c5e57d2e6d9179f3b0eaa73dba846c935
Helene Tindon <htindon@student.42.fr>
2014-11-03 19:53:37 +0100

    Going to work from home, pushing bugged code

---
+++ dungeon.js
@@ -0,0 +1,8 @@
+var dungeon = function() {
+
+	level: function(Nb) {
+	document.getElementById("dungeon").innerHTML = "<p>Welcome to Level 1<p><p>There is one type of ennemy on this level: Skeletons. You can attack them directly with your swords</p>";	
+	},
+	document.getElementById("dungeon").innerHTML = "<p>Welcome to the Ivory Dungeon.</p><p>You are a young warrior equipped with a sword and an armour. There are 5 levels in this dungeon</p><br><form><button type='button', value='Go to level 1', alt='Go to level 1'>Go to Level 1!</button></form>";
+	document.close();
+};
